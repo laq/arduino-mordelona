d71e1dbd86f6d41d30fccefc9afa3d6a7c49f6e0
LAQ <laqp14@gmail.com>
2018-07-16 23:11:04 -0500

    Cleaning code (Refactor)
    * Remove use of sliced_history struct -> replaced by clever use of pointer adition
    	(reusing history array)
    * Add debug_level constant
    * Add methods to print measurement name and value, and plot
    
    TODO:
    Abstract to class
    think if separate name and value makes sense
    Separate files for lcd and lcd_ploting

---
+++ esb/ESB_guide.md
@@ -0,0 +1,46 @@
+#ESP8266 guide
+
+##Links
+###Useful guide 
+https://medium.com/@cgrant/using-the-esp8266-wifi-module-with-arduino-uno-publishing-to-thingspeak-99fc77122e82
+
+###AT commands at
+https://learn.acrobotic.com/datasheets/ESP8266_AT_Commands.pdf
+
+##Used
+cutecom or arduinoIde serial monitor
+-- could not set easily picocom or minicom to use CR+LF (or other unidentified problem)
+
+
+#Useful AT commands
+
+##AT 
+	Test if all Ok (Returns ok)
+
+##AT+CWMODE?
+	See current mode: 1. Station (Connect to AP)
+								 2. AP
+								 3. Station and AP
+
+Using mode 1
+
+##AT+GMR
+	View versions
+
+##AT+CWLAP
+Find networks and list them
+
+##AT+CIFSR
+Show ip of station and AP
+
+##AT+PING
+Do a ping
+
+##AT+CWJAP_CUR="abc","0123456789"
+Connect to access point
+
+##AT+CWJAP_DEF="abc","0123456789"
+Connect to access point and save as default
+Using this for local WiFi
+
+
--- main.ino
+++ main.ino
@@ -5,18 +5,7 @@
 const int char_width = 5;
 const int measurement_history_size = 40;
 const int measurement_history_chars = measurement_history_size / 5; // cuts lower and should have bound of 8
-
-struct Sliced_measurement_history {
-  int plot_char[8][5];
-};
-
-void clean_sliced_measurement_history(Sliced_measurement_history &measurement_history) {
-  for (int i = 0;  i < measurement_history_chars; i++) {
-    for (int j = 0; j < char_width; j++) {
-      measurement_history.plot_char[i][j] = 0;
-    }
-  }
-}
+const int DEBUG_LEVEL = 3;// 0 to 10, 0=No debug
 
 int measurement_history[measurement_history_size];
 
@@ -33,16 +22,7 @@ void add_measurement(int measurement_history[], int measurement) {
   measurement_history[measurement_history_size - 1] = measurement;
 }
 
-Sliced_measurement_history split_measurement_history(int measurement_history[]) {
-  Sliced_measurement_history history;
-  clean_sliced_measurement_history(history);
-  for (int i = 0;  i < measurement_history_chars; i++) {
-    for (int j = 0; j < char_width; j++) {
-      history.plot_char[i][j]=measurement_history[i*char_width+j];
-    }
-  }
-  return history;
-}
+
 
 
 
@@ -52,43 +32,48 @@ void setup() {
   lcd.begin(16, 2);
   lcd.blink();
   lcd.print("Start :)...");
-  delay(1000);
+  delay(2000);
 
 }
 
 void loop() {
   Serial.println("Start");
+
   int latest_measurement;
   latest_measurement = get_measurement();
   Serial.print("Measurement:");
   Serial.println(latest_measurement);
-  print_measurement(latest_measurement);
+  add_measurement(measurement_history, latest_measurement);
+  lcdprint_measurement_name_value(0, 0);
+  lcdprint_measurement_plot(1, 0);
+
   Serial.print("Ram:");
   Serial.println(freeRam());
-  delay(500);
+  delay(1000);
 }
 
-void print_measurement(int latest_measurement) {
-  Serial.println("Printing measurement");
-  add_measurement(measurement_history, latest_measurement);
-  Serial.println("Added Measurement");
 
-  Sliced_measurement_history sliced_measurement_history = split_measurement_history(measurement_history);
-  Serial.println("Sliced measurement history");  
+String measurement_name = "Temperature";
+void lcdprint_measurement_name_value(int col, int row) {
+  lcd.setCursor(row, col);
+  lcd.print(measurement_name);
+  lcd.print(":");
+  lcd.print(measurement_history[measurement_history_size - 1]);
+}
 
 
 
+void lcdprint_measurement_plot(int col, int row) {
   for (int i = 0; i < measurement_history_chars; i++) {
-    printArray(sliced_measurement_history.plot_char[i], 5);
-    LCD_plot_char * plot_char = new LCD_plot_char(sliced_measurement_history.plot_char[i]);
-    plot_char->createChar(i);    
+    if (DEBUG_LEVEL > 5) {
+      printArray(measurement_history + (char_width * i), char_width);
+    }
+    LCD_plot_char * plot_char = new LCD_plot_char(measurement_history + (char_width * i));
+    plot_char->createChar(i);
     delete plot_char;
   }
 
-  lcd.setCursor(11, 0);
-  lcd.print(latest_measurement);
-
-  lcd.setCursor(0, 1);
+  lcd.setCursor(row, col);
   for (int i = 0; i < measurement_history_chars; i++) {
     lcd.write(byte(i));
   }
@@ -106,9 +91,9 @@ void printArray(int array[], int size) {
 }
 
 int freeRam () {
-  extern int __heap_start, *__brkval; 
-  int v; 
-  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
+  extern int __heap_start, *__brkval;
+  int v;
+  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
 }
 
 
